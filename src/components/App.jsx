caba5e974309a2fbe0069a3a69ebb0351cf7f9fc
vsevolodjarkov <94003560+vsevolodjarkov@users.noreply.github.com>
2022-08-08 09:18:49 +0300

    save

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,26 @@
+import { useDispatch, useSelector } from 'react-redux';
+
 export const App = () => {
+  const dispatch = useDispatch();
+  const cash = useSelector(state => state.cash);
+
+  const AddCash = (cash) => {
+    dispatch({ type: 'ADD', payload: cash });
+  };
+  const GetCash = (cash) => {
+    dispatch({ type: 'GET', payload: cash });
+  };
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <div>{cash}</div>
+      <div>
+        <button type="button" onClick={() => AddCash(Number(prompt()))}>
+          +
+        </button>
+        <button type="button" onClick={() => GetCash(Number(prompt()))}>
+          -
+        </button>
+      </div>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,10 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+
 import './index.css';
+import { Provider } from 'react-redux';
+import { createStore } from 'react';
+import { App } from 'components/App';
+const defState = {
+	cash: 0,
+}
+
+const reducer = (state = defState, action) => {
+	switch(action.type){
+		case 'ADD':
+			return { ...state, case: state.cash + action.payload}
+
+		case 'GET':
+			return { ...state, case: state.cash - action.payload}
+
+		default:
+			return state;
+	}
+
+}
+const store = createStore(reducer);
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+		<Provider store={store}>
+			<App />
+		</Provider>
+  </React.StrictMode> 
 );
